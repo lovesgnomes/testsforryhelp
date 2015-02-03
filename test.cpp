21a477308c094a4a686225e6c34de523a09feebb
lovesgnomes <gost2143@yahoo.com>
2015-02-02 21:56:06 -0500

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,16 @@
+# testsforryhelp
+#include <iostream>
+#include "complexNumber.h"
+#include <cstring>
+#include <string.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main () {
+
+
+ComplexNumber cn = "hey \n"; 
+
+return 0;
+}
